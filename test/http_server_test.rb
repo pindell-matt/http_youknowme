4c28f641efa268c20a7d2fdb3287755ae0c99c8d
Deborah Hamel <deborahleehamel@gmail.com>
2016-01-05 18:01:43 -0700

    Cleaned up commented code.

--- test/http_server_test.rb
+++ test/http_server_test.rb
@@ -1,27 +1,17 @@
 require 'simplecov'
 SimpleCov.start
-# require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'socket'
 require 'hurley'
 # require 'pry'
-# require_relative '../lib/iter_0'
 
 class HTTP_Server_Test < Minitest::Test
   attr_reader :client
-
-  # def setup
-  #   @client = Hurley::Client.new "http://127.0.0.1:9292"
-  # end
-
-  ## You can also use Hurley class level shortcuts, which use Hurley.default_client.
-  ## response = Hurley.get("https://api.github.com/users/tater")
 
   def test_server_responds
     response = Hurley.get("http://127.0.0.1:9292")
     assert response.success?
   end
-
 
 end
