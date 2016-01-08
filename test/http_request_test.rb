8fa8607aab810908abb2eaaf96131c927d06333d
Matt Pindell <pindell.m@gmail.com>
2016-01-07 20:14:26 -0700

    updated Request test

--- test/http_request_test.rb
+++ test/http_request_test.rb
@@ -8,4 +8,13 @@ require_relative '../lib/http_request'
 class HTTP_Request_Test < Minitest::Test
   attr_reader :client
 
+  def test_full_request_hello_verb
+    client = Hurley::Client.new("http://127.0.0.1:9292")
+    request = client.get("/hello")
+    expected = "GET"
+    received = request.inspect.split[1]
+    binding.pry
+    assert_equal expected, received
+  end
+
 end
